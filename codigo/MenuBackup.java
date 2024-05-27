c3ec5c815f2faff4144305b0e427b310fd9751d0
LuisAugusto0 <laloaugusto78@gmail.com>
2024-05-27 12:21:17 -0300

    codificação v1.0
    Co-authored-by: Victor Ferraz <vferraz48@gmail.com>

--- codigo/MenuBackup.java
+++ codigo/MenuBackup.java
@@ -1,15 +1,71 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.io.File;
+import java.io.FilenameFilter;
 import aeds3.ArquivoLZW;
 
 public class MenuBackup {
     private static Scanner console = new Scanner(System.in);
 
+    //-------------
+    // Diretorios de dados e complementos 
+    //-------------
+    private static String initialDataPath = "codigo/dados/";
+    private static String restoredComplementPath = "restored/";
+    private static String backupComplementPath = "backups/";
 
     // ---------------------
     // MENU_BACKUP
     // ---------------------
+    public String choseBackupFile() throws Exception{
+        String backupPath = initialDataPath + "backups";
+        File dir = new File(backupPath);
+        String escolha = "";
+    
+        // Verificar se o diretório existe e é de fato um diretório
+        if (dir.exists() && dir.isDirectory()) {
+            // Utilizando FilenameFilter para filtrar os arquivos que começam com "backup-"
+            String[] filteredFiles = dir.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.startsWith("backup-");
+                }
+            });
+            // Imprimindo os arquivos filtrados
+            if (filteredFiles != null) {
+                System.out.println("\n\n\nBOOKAEDS 1.0");
+                System.out.println("------------");
+                System.out.println("\n> Início > Backup > Restauração");
+
+                System.out.println("\nEscolha o número referente ao arquivo que deseja fazer backup: ");
+                //for (String fileName : filteredFiles) {
+                for(int i=0; i<filteredFiles.length; i++){
+                    System.out.println(i+1 + ") " + filteredFiles[i]);
+                }
+                int option;
+                System.out.print("\nOpção: ");
+                try {
+                    option = Integer.valueOf(console.nextLine())-1;
+                } catch (NumberFormatException e) {
+                    option = -1;
+                }
+                if(option >= 0 && option < filteredFiles.length){
+                    escolha = filteredFiles[option];
+                    System.out.println("\nIniciando restauração de " + escolha);
+                } else {
+                    throw new Exception("Opção inválida");
+                }
+            } else {
+                throw new Exception("Nenhum arquivo encontrado com o prefixo 'backup-'.");
+            }
+        } else {
+            throw new Exception("Diretório não existe ou não é um diretório válido.");
+        }
+        
+        return escolha;
+    }
+
     public void menu() throws Exception{
         // Mostra o menu
         int opcao;
@@ -31,10 +87,10 @@ public class MenuBackup {
             // Seleciona a operação
             switch (opcao) {
                 case 1:
-                    (new ArquivoLZW()).codifica("codigo/dados/");
+                    (new ArquivoLZW()).codifica(initialDataPath, backupComplementPath);
                     break;
                 case 2:
-                    (new ArquivoLZW()).decodifica("codigo/dados/backups/backup-1716775483638.lzw");
+                    (new ArquivoLZW()).decodifica(choseBackupFile(), initialDataPath + backupComplementPath, initialDataPath + restoredComplementPath);
                     break;
                 case 0:
                     break;
